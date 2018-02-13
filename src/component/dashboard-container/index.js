69a2e8507f5c49af86c632be493d6d3f98f7bfca
Rubiksron <rubiksron@gmail.com>
2018-02-13 09:54:50 -0800

    changed build folder to docs in webpack so that github can redeploy from heroku as per the set up. I also moved <Skill /> from dashboard to main App.

--- src/component/dashboard-container/index.js
+++ src/component/dashboard-container/index.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import Navbar from '../navbar'
-import Skills from '../skills'
 
 class DashboardContainer extends React.Component {
   render(){
     return (
       <div className='dashboard-container'>
         <Navbar />
-        <Skills />
-        
       </div>
     )
   }
--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import Footer from './component/footer'
 import Router from './component/router-container'
+import Skills from './component/skills'
 
 class App extends React.Component {
   render() {
     return (
       <main className='app'>
       <Router />
+      <Skills />
       <Footer />
       </main>
     )
