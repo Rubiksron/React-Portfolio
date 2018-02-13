38107ef509f997c815e91e45477da510f7bf8390
Rubiksron <rubiksron@gmail.com>
2018-02-13 08:59:37 -0800

    put skills section into main app.

--- src/component/router-container/index.js
+++ src/component/router-container/index.js
@@ -10,14 +10,14 @@ class RouterContainer extends React.Component {
   render() {
     return (
       <MemoryRouter>
-        <div>
-            <Route exact path='/'
-            component={ DashboardContainer } />
-            <Route exact path='/about'
-            component={ AboutContainer } />
-            <Route exact path='/portfolio'
-            component={ PortfolioContainer } />
-          </div>
+        <div>      
+          <Route exact path='/'
+          component={ DashboardContainer } />
+          <Route exact path='/about'
+          component={ AboutContainer } />
+          <Route exact path='/portfolio'
+          component={ PortfolioContainer } />
+        </div>
       </MemoryRouter>
     )
   }
