ac682e08083923dd55952e4cb58a81c409b3333a
Rubiksron <rubiksron@gmail.com>
2018-02-28 20:09:53 -0800

    removed unused icons and added box-shadow to portfolio divs and dashboardlanding.

--- src/component/dashboard-container/index.js
+++ src/component/dashboard-container/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import PortfolioWorking from '../../../images/portfolio-working.jpeg'
+import DashRon from '../../../images/dashboard-ron.jpg'
 
 class DashboardContainer extends React.Component {
   render(){
     return (
       <div className='dashboard-container'>
+        <img src={ DashRon } className="dash-ron"/>
+        <p>My name is Ron Dunphy and I solve puzzles.</p>
         <Link to='/portfolio'><div className="link-to-portfolio"><p className="dash-links">Portfolio</p></div></Link>
         <Link to='/about'><div className="link-to-about"><p className="dash-links">About</p></div></Link>
       </div>
