26182aacd39bfa991fa1beec5c45ef604fdb1958
Rubiksron <rubiksron@gmail.com>
2018-02-28 18:26:48 -0800

    Added background image (sky) also added clickable images to about and portfolio sections. Resized some font.

--- src/component/dashboard-container/index.js
+++ src/component/dashboard-container/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import PortfolioWorking from '../../../images/portfolio-working.jpeg'
 
 class DashboardContainer extends React.Component {
   render(){
     return (
       <div className='dashboard-container'>
+        <Link to='/portfolio'><div className="link-to-portfolio"><p className="dash-links">Portfolio</p></div></Link>
+        <Link to='/about'><div className="link-to-about"><p className="dash-links">About</p></div></Link>
       </div>
     )
   }
