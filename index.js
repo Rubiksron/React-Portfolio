6188c5484f9371080003d72328ad0a4a656ef129
Rubiksron <rubiksron@gmail.com>
2018-09-18 20:11:51 -0700

    I added a c sharp logo and tsql logo to the skills section.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-
+const PORT = 3000;
+//change back to process.env.PORT before pushing to github
 express()
   .use(express.static(`${__dirname}/docs`))
   .get('*', (req, res) => res.sendFile(`${__dirname}/docs/index.html`))
-  .listen(process.env.PORT, () =>
-    console.log('__SERVER_RUNNING__', process.env.PORT));
+  .listen(PORT, () =>
+    console.log('__SERVER_RUNNING__', PORT));
