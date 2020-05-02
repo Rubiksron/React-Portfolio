82bd8220233b0dac160e033a52c4e3b7cc275f45
Rubiksron <rubiksron@gmail.com>
2020-05-02 11:41:43 -0700

    making change to text in intro to test pushability.

--- src/component/dashboard-container/index.js
+++ src/component/dashboard-container/index.js
@@ -10,7 +10,7 @@ class DashboardContainer extends React.Component {
       <div className='dashboard-container'>
         <img src={ DashRon } className="dash-ron" alt="picture-of-ron" />
         <p className="bio">
-          Javascript developer always on the lookout for the next big challenge. I can solve a rubik’s cube in 15 seconds and algorithmic problem solving is in my blood. Aside from years of teaching other nerds how to Speed-Cube by selling custom DVD’s and giving tailored lessons, I’ve camped and hiked my way around the Pacific Northwest. Although a native Washingtonian, I’ve lived across the United States in Alaska, North Carolina, and Los Angeles. A puzzle-solver by nature, I excel at efficiency and adaptability.
+          I am a Javascript developer always on the lookout for the next big challenge. I can solve a rubik’s cube in 15 seconds and algorithmic problem solving is in my blood. Aside from years of teaching other nerds how to Speed-Cube by selling custom DVD’s and giving tailored lessons, I’ve camped and hiked my way around the Pacific Northwest. Although a native Washingtonian, I’ve lived across the United States in Alaska, North Carolina, and Los Angeles. A puzzle-solver by nature, I excel at efficiency and adaptability.
         </p>
         <Link to='/portfolio'><div className="link-to-portfolio"></div></Link>
         <Link to='/about'><div className="link-to-about"></div></Link>
