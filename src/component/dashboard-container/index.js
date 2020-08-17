02ac40c7ab6de474f0c899796760d9b963e755d0
Rubiksron <rubiksron@gmail.com>
2020-08-17 16:07:45 -0700

    adding 'teaching' to assistant.

--- src/component/dashboard-container/index.js
+++ src/component/dashboard-container/index.js
@@ -8,7 +8,7 @@ class DashboardContainer extends React.Component {
       <div className='dashboard-container'>
         <div id="round-ron-main"><img src={DashRon} className="dash-ron" alt="picture-of-ron" /></div>
         <div className="bio">
-          <p>I am a JavaScript developer always on the lookout for the next big challenge. I currently work as a Lead Teaching Assistant for Code Fellows coding academy in downtown Seattle, developing and teaching course curriculum for the advanced full-stack JavaScript certificate that focuses on responsive website and app development. As an assistant I help build and adapt scalable full-stack curriculum, teach students important career skills such as project management and team building, and work directly with each student to build and debug projects.
+          <p>I am a JavaScript developer always on the lookout for the next big challenge. I currently work as a Lead Teaching Assistant for Code Fellows coding academy in downtown Seattle, developing and teaching course curriculum for the advanced full-stack JavaScript certificate that focuses on responsive website and app development. As a teaching assistant I help build and adapt scalable full-stack curriculum, teach students important career skills such as project management and team building, and work directly with each student to build and debug projects.
           </p>
           <p>
             Aside from years of teaching other people how to Speed-Cube by selling custom DVD’s and giving tailored lessons, I’ve camped and hiked my way around the Pacific Northwest. Although a native Washingtonian, I’ve lived across the United States in Alaska, North Carolina, and Los Angeles.
